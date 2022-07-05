feab402686ee6a7ee1d46ecb6fc0657010262021
Guilherme Hachimine <gui.hachimine@gmail.com>
2022-07-05 17:41:52 -0300

    feat: ViewModel implemantation

---
+++ Session 3/Classes/13/myVersion/app/src/main/java/com/example/myversion/MainActivityViewModel.kt
@@ -0,0 +1,14 @@
+package com.example.myversion
+
+import androidx.lifecycle.ViewModel
+
+class MainActivityViewModel : ViewModel(){
+    private var count = 0
+
+    fun getCurrentCount() : Int {
+        return count
+    }
+    fun getUpdatedCount(): Int {
+        return ++count;
+    }
+}
