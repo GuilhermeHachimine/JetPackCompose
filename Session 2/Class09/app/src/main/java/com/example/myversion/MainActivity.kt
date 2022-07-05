3438723dd2b3e5ddd720f8c40e8823c6d089d662
Guilherme Hachimine <gui.hachimine@gmail.com>
2022-07-05 16:30:05 -0300

    feat: progress bar databinding

---
+++ Session 2/Class09/app/src/main/java/com/example/myversion/MainActivity.kt
@@ -0,0 +1,30 @@
+package com.example.myversion
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import androidx.databinding.DataBindingUtil
+import com.example.myversion.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = DataBindingUtil.setContentView(this,R.layout.activity_main)
+        binding.controlButton.setOnClickListener{
+            startOrStopProgressBar()
+        }
+    }
+
+    private fun startOrStopProgressBar() {
+        binding.apply {
+            if(progressBar.visibility == View.GONE) {
+                progressBar.visibility = View.VISIBLE
+                controlButton.text = "Stop"
+            } else {
+                progressBar.visibility = View.GONE
+                controlButton.text="Start"
+            }
+        }
+    }
+}
