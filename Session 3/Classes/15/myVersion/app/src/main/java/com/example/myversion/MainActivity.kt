a0df80000a00174985c8029b6966cac6d930513f
Guilherme Hachimine <gui.hachimine@gmail.com>
2022-07-06 13:57:39 -0300

    feat: mutableLiveData and LiveData

--- Session 3/Classes/15/myVersion/app/src/main/java/com/example/myversion/MainActivity.kt
+++ Session 3/Classes/15/myVersion/app/src/main/java/com/example/myversion/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.myversion
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.example.myversion.databinding.ActivityMainBinding
 
@@ -15,11 +16,12 @@ class MainActivity : AppCompatActivity() {
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
         viewModelFactory = MainActivityViewModelFactory(125)
         viewModel = ViewModelProvider(this,viewModelFactory).get(MainActivityViewModel::class.java)
-        binding.resultTextView.text = viewModel.getTotal().toString()
+        viewModel.totalData.observe(this, Observer {
+            binding.resultTextView.text = it.toString()
+        })
 
         binding.insertButton.setOnClickListener {
             viewModel.setTotal(binding.inputEditText.text.toString().toInt())
-            binding.resultTextView.text = viewModel.getTotal().toString()
         }
     }
 }
